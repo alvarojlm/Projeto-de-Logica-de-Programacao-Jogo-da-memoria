232333ca7499ba785c8e83d46483c510bc3f2dc0
alvarotassio <50063395+alvarotassio@users.noreply.github.com>
2019-04-30 12:43:16 -0300

    Update etapa 1.js

--- etapa 1.js
+++ etapa 1.js
@@ -1,10 +1,15 @@
+/*
+ Equipe:
+ Alvaro Jose Lucena Marinho - subturma D
+ Tassio Araujo de Lima - subturma D
+ Etapa 1
+ */
 var x1 = 100;
-var x2 = 100;
+var x2 = 175;
 var x3 = 100;
-var x4 = 100;
-var x5 = 100;
-var x6 = 200;
-var v1 = 3;
+var v1 = 3.45;
+var v2 = 3;
+var v3 = 2.55;
 
 function setup() {
   createCanvas(900, 900);
@@ -12,20 +17,24 @@ function setup() {
 
 function draw() {
   background(0, 255, 0);
-  rect(x1, 20, 60, 60);
+  rect(x1, 80, 220, 220);
   x1 = x1 + v1;
 if (x1 > 800) {
-    x1 = 100;
-  v1 =v1+5
+    x1 = 50;
+  v1 =v1 + 1;
   
   }
-  circle(x2, 140, 35, 60, 60);
-  x2 = x2 + 10;
+  circle(x2, 480, 110);
+  x2 = x2 + v2;
   if (x2 > 800) {
-    x2 = 100;
+    x2 = 50;
+    v2 = v2 +1;
   }
     
-   triangle( x6, 180, x6+100, 100, x6+200, 180)
-  x6 = x6+2
-  if (x6 > 800)
-x6=100  
+   triangle( x3, 813, x3+110, 630, x3+220, 813)
+  x3 = x3+v3
+  if (x3 > 800){
+  x3=50;
+  v3= v3 + 1.2;
+  }
+}

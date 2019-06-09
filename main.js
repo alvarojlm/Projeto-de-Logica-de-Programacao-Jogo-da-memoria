a6c52d18442c962cf105fbb6d0644f39319f26f0
alvarotassio <50063395+alvarotassio@users.noreply.github.com>
2019-06-08 22:16:32 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,74 @@
+const cardBoard = document.querySelector("#cardboard");
+const images = [
+  "circulo.JPG",
+  "areacirculo.JPG",
+  "quadrado.JPG",
+  "triangulo.JPG",
+  "trapezio.JPG",
+  "areatriangulo.JPG"
+];
+
+let cardHTML = "";
+
+images.forEach(img => {
+  cardHTML += `<div class="memory-card" data-card="${img}">
+    <img class="front-face" src="img/${img}"/>
+    <img class="back-face" src="img/titulo.png">
+  </div>`;
+});
+
+cardBoard.innerHTML = cardHTML + cardHTML;
+
+/** Fim da Renderização HTML */
+
+const cards = document.querySelectorAll(".memory-card");
+let firstCard, secondCard;
+let lockCards = false;
+
+function flipCard() {
+  if (lockCards) return false;
+  this.classList.add("flip");
+
+  if (!firstCard) {
+    firstCard = this;
+    return false;
+  }
+
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+  !isMatch ? disableCards() : resetCards(isMatch);
+}
+
+function disableCards() {
+  lockCards = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetCards();
+  }, 1000);
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    })
+})();
+
+function resetCards(isMatch = false) {
+  if (isMatch) {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+  }
+
+  [firstCard, secondCard, lockCards] = [null, null, false];
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard));
